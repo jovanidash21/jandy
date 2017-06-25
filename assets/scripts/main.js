e5daddb2b184de1d1351eae7395fb393decddedc
Jovani Cadorna Warguez <jwardash21@gmail.com>
2017-06-25 18:06:42 +0800

    Update Nav Bar Section

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,11 +1,32 @@
 $(document).ready(function() {
-  var scrollHeight = $(window).scrollTop();
-
   // Sections
   var headerSection = $('.header-section');
   var navBarSection = $('.nav-bar-section');
   var heroSliderSection = $('.hero-slider-section');
 
+  // Heights
+  var windowHeight = $(window).height();
+  var htmlHeight = $(document).height();
+  var headerSectionHeight = navBarSection.offset().top;
+
+  // Window Scroll
+  $(window).scroll(function() {
+    var scrollHeight = $(window).scrollTop();
+    var navBar = navBarSection.find('nav.navbar');
+
+    navBar.wrap('<div class="nav-bar-section-wrapper"></div>');
+    navBarSection.find('.nav-bar-section-wrapper').height(navBarSection.outerHeight());
+
+    if (scrollHeight >= headerSectionHeight) {
+      navBar.addClass('navbar-fixed-top no-border').autoHidingNavbar({
+        hideOffset: htmlHeight/3
+      });
+    } else {
+      navBar.removeClass('navbar-fixed-top no-border');
+    }
+  });
+
+  // Nav bar section
   navBarSection.find('a.dropdown-toggle').hover(function(e) {
     var _this = $(this);
     var parent = _this.offsetParent(".dropdown-menu");
@@ -20,6 +41,7 @@ $(document).ready(function() {
     return false;
   });
 
+  // Hero slider section
   heroSliderSection.find('.owl-carousel').owlCarousel({
     autoplay: true,
     autoplayHoverPause: false,
