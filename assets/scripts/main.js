7bba41f88844cb6c989f2abe28a8d1cd4a727b80
Jovani Cadorna Warguez <jwardash21@gmail.com>
2017-06-25 14:24:41 +0800

    Update Main JS

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,19 +1,26 @@
 $(document).ready(function() {
-  $('.nav-bar-section a.dropdown-toggle').hover(function(e) {
-    var $el = $(this);
-    var $parent = $(this).offsetParent(".dropdown-menu");
-    $(this).parent("li").toggleClass('open');
+  var scrollHeight = $(window).scrollTop();
 
-    if(!$parent.parent().hasClass('nav')) {
-        $el.next().css({"top": $el[0].offsetTop, "left": $parent.outerWidth() - 4});
+  // Sections
+  var headerSection = $('.header-section');
+  var navBarSection = $('.nav-bar-section');
+  var heroSliderSection = $('.hero-slider-section');
+
+  navBarSection.find('a.dropdown-toggle').hover(function(e) {
+    var _this = $(this);
+    var parent = _this.offsetParent(".dropdown-menu");
+    _this.parent("li").toggleClass('open');
+
+    if(!parent.parent().hasClass('nav')) {
+        _this.next().css({"top": _this[0].offsetTop, "left": parent.outerWidth() - 4});
     }
 
-    $('.nav li.open').not($(this).parents("li")).removeClass("open");
+    $('.nav li.open').not(_this.parents("li")).removeClass("open");
 
     return false;
   });
 
-  $('.hero-slider-section .owl-carousel').owlCarousel({
+  heroSliderSection.find('.owl-carousel').owlCarousel({
     autoplay: true,
     autoplayHoverPause: false,
     autoplayTimeout: 5000,
