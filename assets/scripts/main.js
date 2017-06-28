5b195f000ec39df6995aaca6b3878a036be78b1b
Jovani Cadorna Warguez <jwardash21@gmail.com>
2017-06-28 13:43:21 +0800

    Update Main JS

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,31 +1,27 @@
-$(document).ready(function() {
-  // Sections
-  var headerSection = $('.header-section');
-  var navBarSection = $('.nav-bar-section');
-  var heroSliderSection = $('.hero-slider-section');
+// Sections
+var headerSection = $('.header-section');
+var navBarSection = $('.nav-bar-section');
+var heroSliderSection = $('.hero-slider-section');
 
-  // Heights
-  var windowHeight = $(window).height();
+$(window).scroll(function() {
+  var scrollHeight = $(window).scrollTop();
   var htmlHeight = $(document).height();
   var headerSectionHeight = navBarSection.offset().top;
+  var navBar = navBarSection.find('nav.navbar');
 
-  // Window Scroll
-  $(window).scroll(function() {
-    var scrollHeight = $(window).scrollTop();
-    var navBar = navBarSection.find('nav.navbar');
+  navBar.wrap('<div class="nav-bar-section-wrapper"></div>');
+  navBarSection.find('.nav-bar-section-wrapper').height(navBarSection.outerHeight());
 
-    navBar.wrap('<div class="nav-bar-section-wrapper"></div>');
-    navBarSection.find('.nav-bar-section-wrapper').height(navBarSection.outerHeight());
+  if (scrollHeight >= headerSectionHeight) {
+    navBar.addClass('navbar-fixed-top no-border').autoHidingNavbar({
+      hideOffset: htmlHeight/3
+    });
+  } else {
+    navBar.removeClass('navbar-fixed-top no-border');
+  }
+});
 
-    if (scrollHeight >= headerSectionHeight) {
-      navBar.addClass('navbar-fixed-top no-border').autoHidingNavbar({
-        hideOffset: htmlHeight/3
-      });
-    } else {
-      navBar.removeClass('navbar-fixed-top no-border');
-    }
-  });
-
+$(document).ready(function() {
   // Lightbox Config
   lightbox.option({
     disableScrolling: true
