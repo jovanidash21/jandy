6c1170a6fbc1525db30fe337370d78584506b6bf
Jovani Cadorna Warguez <jwardash21@gmail.com>
2017-07-02 16:54:09 +0800

    Delete Header Section

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -25,24 +25,6 @@ var headerSection = $('.header-section');
 var navBarSection = $('.nav-bar-section');
 var heroSliderSection = $('.hero-slider-section');
 
-$(window).scroll(function() {
-  var scrollHeight = $(window).scrollTop();
-  var htmlHeight = $(document).height();
-  var headerSectionHeight = navBarSection.offset().top;
-  var navBar = navBarSection.find('nav.navbar');
-
-  navBar.wrap('<div class="nav-bar-section-wrapper"></div>');
-  navBarSection.find('.nav-bar-section-wrapper').height(navBarSection.outerHeight());
-
-  if (scrollHeight >= headerSectionHeight) {
-    navBar.addClass('navbar-fixed-top no-border').autoHidingNavbar({
-      hideOffset: htmlHeight/3
-    });
-  } else {
-    navBar.removeClass('navbar-fixed-top no-border');
-  }
-});
-
 $(document).ready(function() {
   // Lightbox Config
   lightbox.option({
