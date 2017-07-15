500ab27d67f9e4aee57d31655ce677a63bbbcfd5
Jovani Cadorna Warguez <jwardash21@gmail.com>
2017-07-15 18:03:16 +0800

    Update Gulp Watch Task

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -9,7 +9,7 @@ gulp.task('watch', ['webserver'], function () {
     });
     gulp.watch([config.assets + 'views/**/*'], ['views']);
     gulp.watch([config.assets + 'styles/**/*'], ['styles']);
-    gulp.watch([config.assets + 'scripts/**/*'], ['jshint', 'scripts']);
+    gulp.watch([config.assets + 'scripts/**/*'], ['lint', 'scripts']);
     gulp.watch([config.assets + 'images/**/*'], ['images']);
     gulp.watch([config.assets + 'fonts/**/*'], ['fonts']);
     gulp.watch(['bower.json', 'assets/manifest.json'], ['build']);
